6e883375109488e7e7e7c72d097dab77f308a458
micker7 <mickee.birti@gmail.com>
2023-07-13 22:39:58 -0400

    edits

---
+++ SalesTaxes/SalesTaxes/Cart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+public class Cart
+{
+    public static void PrintReceipt(List<string> items, List<double> costs, List<int> quantity)
+    {
+
+      
+        Console.WriteLine("Items:\t\t\tPrices:");
+        Console.WriteLine("----------------------------------------------");
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            Console.WriteLine($"{items[i]}:\t{costs[i]:C}");
+        }
+
+        if()
+
+    }
+
+    public static double TotalCosts(List<double> costs)
+    {
+        double totalCost = costs.Sum();
+        return totalCost;
+
+
+    }
+}
+
+
---
+++ SalesTaxes/SalesTaxes/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Helper
+{
+   
+    public static double TaxRate(List<string> items, List<double> costs)
+    {
+        double taxRate;
+
+        if (items.Contains("book") || items.Contains("chocolate") || items.Contains("pills"))
+        {
+            taxRate = 0.00;
+        }
+        else
+        {
+            taxRate = 0.1;
+        }
+
+        if (items.Contains("imported"))
+        {
+            taxRate += 0.05;
+        }
+
+        return taxRate;
+
+    }
+}
+
+
+
+
